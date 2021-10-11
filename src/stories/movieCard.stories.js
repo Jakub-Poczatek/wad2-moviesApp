6cb98ff8e8c53ed2b7f7a128c4d5e9a1b6822d71
Jakub-Poczatek <kubapoczatek2001@gmail.com>
2021-10-11 16:53:12 +0100

    Home page components.

---
+++ src/stories/movieCard.stories.js
@@ -0,0 +1,23 @@
+import React from "react";
+import MovieCard from "../components/movieCard";
+import SampleMovie from "./sampleData";
+
+export default {
+    title: "Home Page/Movie Card",
+    component: MovieCard,
+};
+
+export const Basic = () => {
+    return (
+        <MovieCard movie = {SampleMovie} />
+    );
+};
+Basic.storyName = "Default";
+
+export const Exceptional = () => {
+    const sampleNoPoster = { ...SampleMovie, poster_path: undefined}
+    return(
+        <MovieCard movie = {sampleNoPoster} />
+    );
+};
+Exceptional.storyName = "exception";
