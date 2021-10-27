6ae40f9cace1b210d69131d50ffeb961bf791da6
Jakub-Poczatek <kubapoczatek2001@gmail.com>
2021-10-27 22:34:07 +0100

    Show movie review excerpts; Refactor Details page.

--- src/api/tmdb-api.js
+++ src/api/tmdb-api.js
@@ -29,3 +29,13 @@ export const getMovies = () => {
       .then((res) => res.json())
       .then((json) => json.posters);
   };
+
+  export const getMovieReviews = (id) => {
+    return fetch(
+      `https://api.themoviedb.org/3/movie/${id}/reviews?api_key=${process.env.REACT_APP_TMDB_KEY}`
+    )
+    .then ((res) => res.json())
+    .then ((json) => {
+      return json.results;
+    });
+  };
--- src/components/movieDetails/index.js
+++ src/components/movieDetails/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import Chip from "@material-ui/core/Chip";
 import Paper from "@material-ui/core/Paper";
 import AccessTimeIcon from "@material-ui/icons/AccessTime";
@@ -9,6 +9,8 @@ import Fab from "@material-ui/core/Fab";
 import Typography from "@material-ui/core/Typography";
 import { makeStyles } from "@material-ui/core/styles";
 import { MovieSharp } from "@material-ui/icons";
+import Drawer from "@material-ui/core/Drawer";
+import MovieReviews from "../movieReviews";
 
 const useStyles = makeStyles((theme) => ({
 
@@ -31,9 +33,9 @@ const useStyles = makeStyles((theme) => ({
 
   }));
 
-  const MovieDetails = (props) => {
+  const MovieDetails = ({movie}) => {
     const classes = useStyles();
-    const movie = props.movie
+    const [drawerOpen, setDrawerOpen] = useState(false);
   
     return (
       <>
@@ -81,12 +83,17 @@ const useStyles = makeStyles((theme) => ({
         <Fab
           color="secondary"
           variant="extended"
+          onClick={() => setDrawerOpen(true)}
           className={classes.fab}
         >
           <NavigationIcon />
           Reviews
         </Fab>
+        <Drawer anchor = "top" open = {drawerOpen} onClose = {() => setDrawerOpen(false)}>
+          <MovieReviews movie={movie}/>
+        </Drawer>
         </>
     );
   };
+  
   export default  MovieDetails ;
