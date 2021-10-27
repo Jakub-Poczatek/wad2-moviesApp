5f4fe360609118bd4b7cc912d2bf9419635adb55
Jakub-Poczatek <kubapoczatek2001@gmail.com>
2021-10-27 23:46:22 +0100

    Added Upcoming movies page.

--- src/api/tmdb-api.js
+++ src/api/tmdb-api.js
@@ -5,6 +5,14 @@ export const getMovies = () => {
       .then(res => res.json())
       .then(json => json.results);
   };
+
+export const getUpcomingMovies = () => {
+  return fetch(
+    `https://api.themoviedb.org/3/movie/upcoming?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US&page=1`
+  )
+  .then(res => res.json())
+  .then(json => json.results);
+  };
   
   export const getMovie = id => {
     return fetch(
