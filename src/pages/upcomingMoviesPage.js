dab8e39c59499694b227c65e7fef2ffd90d0b6fb
Jakub-Poczatek <kubapoczatek2001@gmail.com>
2021-10-28 13:43:43 +0100

    Fix upcoming movies page.

--- src/pages/upcomingMoviesPage.js
+++ src/pages/upcomingMoviesPage.js
@@ -1,33 +1,36 @@
-import React, { useState, useEffect } from "react";
-import PageTemplate from '../components/templateMovieListPage'
-import {getUpcomingMovies} from "../api/tmdb-api";
+import React from "react";
+import PageTemplate from "../components/templateMovieListPage";
+import { useQuery } from 'react-query'
+import Spinner from '../components/spinner'
+import {getUpcomingMovies} from '../api/tmdb-api'
+import AddToFavoritesIcon from "../components/cardIcons/addToFavorites";
 
+const HomePage = (props) => {
+  const {  data, error, isLoading, isError }  = useQuery('upcoming', getUpcomingMovies)
 
-const UpcomingMoviesPage = (props) => {
-    const [movies, setMovies] = useState([]);
-    const favorites = movies.filter(m => m.favorite)
-    localStorage.setItem('favorites', JSON.stringify(favorites))
-  
-    const addToFavorites = (movieId) => {
-      const updatedMovies = movies.map((m) =>
-        m.id === movieId ? { ...m, favorite: true } : m
-      );
-      setMovies(updatedMovies);
-    };
-  
-    useEffect(() => {
-      getUpcomingMovies().then(movies => {
-        setMovies(movies);
-      });
-      // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, []);
-  
-    return (
-      <PageTemplate
-        title='Upcoming Movies'
-        movies={movies}
-        selectFavorite={addToFavorites}
-      />
-    );
-  };
-  export default UpcomingMoviesPage;
+  if (isLoading) {
+    return <Spinner />
+  }
+
+  if (isError) {
+    return <h1>{error.message}</h1>
+  }  
+  const movies = data.results;
+
+  // Redundant, but necessary to avoid app crashing.
+  const favorites = movies.filter(m => m.favorite)
+  localStorage.setItem('favorites', JSON.stringify(favorites))
+  const addToFavorites = (movieId) => true 
+
+  return (
+    <PageTemplate
+      title="Upcoming Movies"
+      movies={movies}
+      action={(movie) => {
+        return <AddToFavoritesIcon movie={movie}/>
+      }}
+    />    
+  );
+};
+
+export default HomePage;
