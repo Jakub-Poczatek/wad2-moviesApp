de84e7c962df091618311242261759c2b4048674
Jakub Poczatek <kubapoczatek2001@gmail.com>
2021-12-11 23:56:08 +0000

    Popular Tv Details Page started

---
+++ src/pages/showDetailsPage.js
@@ -0,0 +1,66 @@
+import React from "react";
+import ShowHeader from "../components/showHeader/";
+import ShowDetails from "../components/showDetails/";
+import Grid from "@material-ui/core/Grid";
+import { makeStyles } from "@material-ui/core/styles";
+import GridList from "@material-ui/core/GridList";
+import GridListTile from "@material-ui/core/GridListTile";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: "flex",
+    flexWrap: "wrap",
+    justifyContent: "space-around",
+  },
+  gridList: {
+    width: 450,
+    height: "100vh",
+  },
+}));
+
+const ShowPage = (props) => {
+  const classes = useStyles();
+  const show = props.show;
+  const images = props.images;
+
+  return (
+    <>
+      {show ? (
+        <>
+          <ShowHeader show={show} />
+          <Grid container spacing={5} style={{ padding: "15px" }}>
+            <Grid item xs={3}>
+              <div className={classes.root}>
+                <GridList
+                  cellHeight={500}
+                  className={classes.gridList}
+                  cols={1}
+                >
+                  {images.map((image) => (
+                    <GridListTile
+                      key={image.file_path}
+                      className={classes.gridListTile}
+                      cols={1}
+                    >
+                      <img
+                        src={`https://image.tmdb.org/t/p/w500/${image}`}
+                        alt={image.poster_path}
+                      />
+                    </GridListTile>
+                  ))}
+                </GridList>
+              </div>
+            </Grid>
+            <Grid item xs={9}>
+              <ShowDetails show={show} />
+            </Grid>
+          </Grid>
+        </>
+      ) : (
+        <h2>Waiting for API data</h2>
+      )}
+    </>
+  );
+};
+
+export default ShowPage;
